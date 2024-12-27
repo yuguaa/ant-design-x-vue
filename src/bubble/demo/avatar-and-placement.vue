6ba5b363bb24d37894751997a65857e223c7619a
wzc520pyfm <1528857653@qq.com>
2024-12-27 21:34:56 +0800

    docs(bubble): add avatar and placement demo

---
+++ src/bubble/demo/avatar-and-placement.vue
@@ -0,0 +1,46 @@
+<script setup lang="tsx">
+import { Bubble } from '../../index';
+import { UserOutlined } from '@ant-design/icons-vue';
+import { Flex } from 'ant-design-vue';
+import type { CSSProperties } from 'vue';
+
+defineOptions({ name: 'BubbleAvatarAndPlacement' });
+
+const fooAvatar: CSSProperties = {
+  color: '#f56a00',
+  backgroundColor: '#fde3cf',
+};
+
+const barAvatar: CSSProperties = {
+  color: '#fff',
+  backgroundColor: '#87d068',
+};
+
+const hideAvatar: CSSProperties = {
+  visibility: 'hidden',
+};
+
+defineRender(() => {
+  return (
+    <Flex gap="middle" vertical>
+      <Bubble
+        placement="start"
+        content="Good morning, how are you?"
+        avatar={{ icon: <UserOutlined />, style: fooAvatar }}
+      />
+      <Bubble
+        placement="start"
+        content="What a beautiful day!"
+        styles={{ avatar: hideAvatar }}
+        avatar={{}}
+      />
+      <Bubble
+        placement="end"
+        content="Hi, good morning, I'm fine!"
+        avatar={{ icon: <UserOutlined />, style: barAvatar }}
+      />
+      <Bubble placement="end" content="Thank you!" styles={{ avatar: hideAvatar }} avatar={{}} />
+    </Flex>
+  )
+});
+</script>
