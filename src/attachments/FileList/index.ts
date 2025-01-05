cf91a3c41f674c3d4c08c8506d23a75ab74c4721
wzc520pyfm <1528857653@qq.com>
2025-01-05 16:22:31 +0800

    feat(attachments): export file list component

--- src/attachments/FileList/index.ts
+++ src/attachments/FileList/index.ts
@@ -1,5 +1,7 @@
 import FileList from './FileList.vue';
+import FileListCard from './FileListCard.vue';
 
 export {
-  FileList
+  FileList,
+  FileListCard
 }
--- src/attachments/index.ts
+++ src/attachments/index.ts
@@ -1,4 +1,9 @@
-import Attachments from './Attachments.vue';
+import AttachmentsComponent from './Attachments.vue';
+import { FileListCard } from './FileList';
+
+const Attachments = Object.assign(AttachmentsComponent, {
+  FileCard: FileListCard,
+});
 
 export type { AttachmentsProps } from './interface';
 
