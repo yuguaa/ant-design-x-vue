a9ba6e4489d80966b9d9bacea865a4eea99345ab
linhf <linhf123>
2025-01-11 19:40:40 +0800

    feat(Sender): Add SenderHeader

---
+++ src/sender/SenderHeader.vue
@@ -0,0 +1,79 @@
+<script setup lang="tsx" generic="T = any">
+import { CloseOutlined } from '@ant-design/icons-vue';
+import { Button } from 'ant-design-vue';
+import classNames from 'classnames';
+import type { SenderHeaderProps } from './interface';
+import { useSenderHeaderContextInject } from './context';
+import { computed } from 'vue';
+
+defineOptions({ name: 'AXSenderHeader' });
+
+const {
+  title,
+  onOpenChange,
+  open,
+  children,
+  className,
+  style,
+  classNames: classes = {},
+  styles = {},
+  closable,
+  forceRender,
+} = defineProps<SenderHeaderProps>();
+
+const SendHeaderContext = useSenderHeaderContextInject()
+
+const headerCls = computed(() => `${SendHeaderContext.value.prefixCls}-header`)
+
+defineRender(() => {
+  return (
+    <div
+      class={classNames(headerCls.value, className)}
+      style={{
+        ...style,
+      }}
+    >
+      {/* Header */}
+      {(closable !== false || title) && (
+        <div
+          class={
+            // We follow antd naming standard here.
+            // So the header part is use `-header` suffix.
+            // Though its little bit weird for double `-header`.
+            classNames(`${headerCls.value}-header`, classes.header)
+          }
+          style={{
+            ...styles.header,
+          }}
+        >
+          <div class={`${headerCls.value}-title`}>{title}</div>
+          {closable !== false && (
+            <div class={`${headerCls.value}-close`}>
+              <Button
+                type="text"
+                icon={<CloseOutlined />}
+                size="small"
+                onClick={() => {
+                  onOpenChange?.(!open);
+                }}
+              />
+            </div>
+          )}
+        </div>
+      )}
+
+      {/* Content */}
+      {children && (
+        <div
+          class={classNames(`${headerCls.value}-content`, classes.content)}
+          style={{
+            ...styles.content,
+          }}
+        >
+          {children}
+        </div>
+      )}
+    </div>
+  )
+});
+</script>
