a2fd6465e9ebd90985a1fa980f2a5da6b58d4dbd
linhf <linhf123>
2025-01-12 15:34:44 +0800

    docs(sender): Add header demo

---
+++ docs/examples/sender/header.vue
@@ -0,0 +1,65 @@
+<script setup lang="tsx">
+import { CloudUploadOutlined, LinkOutlined } from '@ant-design/icons-vue';
+import { App, Button, Flex, theme, Typography } from 'ant-design-vue';
+import { Sender } from 'ant-design-x-vue';
+import { ref } from 'vue';
+
+defineOptions({ name: 'AXSenderBasic' });
+
+const Demo = () => {
+  const { message } = App.useApp();
+  const { token } = theme.useToken();
+  const open = ref(false);
+
+  const headerNode = (
+    <Sender.Header title="Upload Sample" open={open.value} onOpenChange={v => open.value = v}>
+      <Flex vertical align="center" gap="small" style={{ marginBlock: token.value.paddingLG }}>
+        <CloudUploadOutlined style={{ fontSize: '4em' }} />
+        <Typography.Title level={5} >
+          Drag file here(just demo)
+        </Typography.Title>
+        <Typography.Title type="secondary" >
+          Support pdf, doc, xlsx, ppt, txt, image file types
+        </Typography.Title >
+        < Button
+          onClick={() => {
+            message.info('Mock select file');
+          }}
+        >
+          Select File
+        </Button>
+      </Flex>
+    </Sender.Header>
+  );
+
+  return (
+    <Flex style={{ height: 350 }} align="end" >
+      <Sender
+        header={headerNode}
+        prefix={
+          < Button
+            type="text"
+            icon={< LinkOutlined />}
+            onClick={() => {
+              open.value = !open.value
+            }}
+          />
+        }
+        placeholder="← Click to open"
+        onSubmit={() => {
+          message.success('Send message successfully!');
+        }}
+      />
+    </Flex>
+  );
+};
+
+defineRender(() => {
+  return (
+    <App>
+      <Demo />
+    </App>
+  )
+});
+
+</script>
