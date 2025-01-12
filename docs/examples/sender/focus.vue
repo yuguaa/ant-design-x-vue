a3374e700e4bd4a75ff608579ad8e8eb2cbeebf3
linhf <linhf123>
2025-01-12 16:12:16 +0800

    docs(sender): add focus demo

---
+++ docs/examples/sender/focus.vue
@@ -0,0 +1,77 @@
+<script setup lang="tsx">
+import { App, Button, Flex } from 'ant-design-vue';
+import { Sender } from 'ant-design-x-vue';
+import { ref } from 'vue';
+
+defineOptions({ name: 'AXSenderBasic' });
+
+const Demo = () => {
+  const senderRef = ref(null);
+
+  const senderProps = {
+    defaultValue: 'Hello, welcome to use Ant Design X!',
+    // TODO: ref 获取不到值
+    ref: senderRef,
+  };
+
+  return (
+    <Flex wrap={'wrap'} gap={12} >
+      <Button
+        onClick={
+          () => {
+            senderRef.value!.focus({
+              cursor: 'start',
+            });
+          }
+        }
+      >
+        Focus at first
+      </Button>
+      <Button
+        onClick={() => {
+          senderRef.value!.focus({
+            cursor: 'end',
+          });
+        }}
+      >
+        Focus at last
+      </Button>
+      <Button
+        onClick={() => {
+          senderRef.value!.focus({
+            cursor: 'all',
+          });
+        }}
+      >
+        Focus to select all
+      </Button>
+      <Button
+        onClick={() => {
+          senderRef.value!.focus({
+            preventScroll: true,
+          });
+        }}
+      >
+        Focus prevent scroll
+      </Button>
+      <Button
+        onClick={() => {
+          senderRef.value!.blur();
+        }}
+      >
+        Blur
+      </Button>
+      <Sender {...senderProps} />
+    </Flex>
+  );
+};
+
+defineRender(() => {
+  return (
+    <App>
+      <Demo />
+    </App>
+  )
+});
+
+</script>
