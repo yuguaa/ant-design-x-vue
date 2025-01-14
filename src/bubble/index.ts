556c2a0360fa5004b587659b6a2ac4555bbaabe5
wzc520pyfm <1528857653@qq.com>
2025-01-14 20:43:57 +0800

    feat(bubble): export Bubble.List

--- src/bubble/index.ts
+++ src/bubble/index.ts
@@ -1,7 +1,11 @@
-import Bubble from './Bubble.vue';
+import BubbleComponent from './Bubble.vue';
 import BubbleList from './BubbleList.vue';
 
 export type { BubbleProps, BubbleListProps } from './interface';
+
+const Bubble = Object.assign(BubbleComponent, {
+  List: BubbleList,
+});
 
 export default Bubble;
 
