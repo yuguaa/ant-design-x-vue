82ecb0cda7e7e4ef4758a9d084692259a27b94cc
归谜 <vhxubo@gmail.com>
2025-03-07 09:07:12 +0800

    docs: add use-x-agent setup usage example (#137)

--- docs/examples-setup/use-x-agent/custom.vue
+++ docs/examples-setup/use-x-agent/custom.vue
@@ -1,6 +1,91 @@
 <script setup lang="ts">
+import { Button, Divider, Form, Input, Typography } from 'ant-design-vue';
+import { useXAgent } from 'ant-design-x-vue';
+import { reactive, ref } from 'vue';
+
 defineOptions({ name: 'AXUseXAgentCustomSetup' });
+
+const lines = ref<string[]>([]);
+
+const modelRef = reactive({
+  question: '',
+});
+
+Form.useForm(modelRef);
+
+const log = (text: string) => {
+  lines.value = [...lines.value, text];
+};
+
+const [ agent ] = useXAgent({
+  request: ({ message }, { onUpdate, onSuccess }) => {
+    let times = 0;
+
+    const id = setInterval(() => {
+      times += 1;
+      onUpdate(`Thinking...(${times}/3)`);
+
+      if (times >= 3) {
+        onSuccess(`It's funny that you ask: ${message}`);
+        clearInterval(id);
+      }
+    }, 500);
+  },
+});
+
+const onFinish = ({ question }: { question: string }) => {
+  log(`[You] Ask: ${question}`);
+  agent.value.request(
+    { message: question },
+    {
+      onUpdate: (message) => {
+        log(`[Agent] Update: ${message}`);
+      },
+      onSuccess: (message) => {
+        log(`[Agent] Answer: ${message}`);
+        modelRef.question = ''
+      },
+      // Current demo not use error
+      onError: () => { },
+    },
+  );
+};
+
 </script>
 <template>
-  <div>Needs to be supplemented.</div>
+  <Form
+    :model="modelRef"
+    layout="vertical"
+    auto-complete="off"
+    @finish="onFinish"
+  >
+    <Form.Item
+      label="Question"
+      name="question"
+    >
+      <Input v-model:value="modelRef.question" />
+    </Form.Item>
+    <Form.Item>
+      <Button
+        html-type="submit"
+        type="primary"
+        :loading="agent.isRequesting()"
+      >
+        submit
+      </Button>
+    </Form.Item>
+  </Form>
+
+  <Divider />
+
+  <Typography>
+    <ul :style="{listStyle: 'circle', paddingLeft: 0 }">
+      <li
+        v-for="(line, index) in lines"
+        :key="index"
+      >
+        {{ line }}
+      </li>
+    </ul>
+  </Typography>
 </template>
--- docs/examples-setup/use-x-agent/preset.vue
+++ docs/examples-setup/use-x-agent/preset.vue
@@ -1,6 +1,111 @@
 <script setup lang="ts">
+import { LoadingOutlined, TagsOutlined } from '@ant-design/icons-vue';
+import { Button, Descriptions, Flex } from 'ant-design-vue';
+import {
+  ThoughtChain,
+  useXAgent,
+  type ThoughtChainItem,
+} from 'ant-design-x-vue';
+import { ref, h } from 'vue';
+
 defineOptions({ name: 'AXUseXAgentPresetSetup' });
+
+/**
+ * 🔔 Please replace the BASE_URL, PATH, MODEL, API_KEY with your own values.
+ */
+const BASE_URL = 'https://api.example.com';
+const PATH = '/chat';
+const MODEL = 'gpt-3.5-turbo';
+/** 🔥🔥 Its dangerously! */
+// const API_KEY = '';
+
+interface YourMessageType {
+  role: string;
+  content: string;
+}
+
+const status = ref<ThoughtChainItem['status']>();
+const lines = ref<Record<string, string>[]>([]);
+
+const [agent] = useXAgent<YourMessageType>({
+  baseURL: BASE_URL + PATH,
+  model: MODEL,
+  // dangerouslyApiKey: API_KEY
+});
+
+async function request() {
+  status.value = 'pending';
+
+  agent.value.request(
+    {
+      messages: [{ role: 'user', content: 'hello, who are u?' }],
+      stream: true,
+    },
+    {
+      onSuccess: (messages) => {
+        status.value = 'success';
+        console.log('onSuccess', messages);
+      },
+      onError: (error) => {
+        status.value = 'error';
+        console.error('onError', error);
+      },
+      onUpdate: (msg) => {
+        // @ts-expect-error
+        lines.value = [...lines.value, msg];
+        console.log('onUpdate', msg);
+      },
+    },
+  );
+}
 </script>
+
 <template>
-  <div>Needs to be supplemented.</div>
+  <Flex
+    align="start"
+    :gap="16"
+    :style="{ overflow: 'auto' }"
+  >
+    <div>
+      <Button
+        type="primary"
+        :disabled="status === 'pending'"
+        @click="request"
+      >
+        Agent Request
+      </Button>
+    </div>
+    <div>
+      <ThoughtChain
+        :style="{ marginLeft: 16 }"
+        :items="[
+          {
+            title: 'Agent Request Log',
+            status: status,
+            icon: status === 'pending' ? h(LoadingOutlined) : h(TagsOutlined),
+            description:
+              status === 'error' &&
+              agent.config.baseURL === BASE_URL + PATH &&
+              'Please replace the BASE_URL, PATH, MODEL, API_KEY with your own values.',
+            content: h(Descriptions, { column: 1 }, [
+              h(
+                Descriptions.Item,
+                {
+                  label: 'Status',
+                },
+                () => status || '-',
+              ),
+              h(
+                Descriptions.Item,
+                {
+                  label: 'Update Times',
+                },
+                () => lines.length,
+              ),
+            ]),
+          },
+        ]"
+      />
+    </div>
+  </Flex>
 </template>
